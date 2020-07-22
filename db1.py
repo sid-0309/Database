78a8df7f80608a17802af215ba046e08d3741749
sid-0309 <sid-0309@users.noreply.github.com>
2020-07-22 23:06:37 +0530

    Update db1.py

--- db1.py
+++ db1.py
@@ -1,15 +1,31 @@
 import csv
 
+global f
 
-def csv_open(x):
-    a = open(x + ".csv", "w")
-    a.close()
 
+def csv_open(a):
+    return open(a + ".csv", "w")
+
+
+#def csv_close():
+#    return close()
+
+def write(a, b):
+    f = csv_open(a)
+    csvwriter = csv.writer(f)
+    csvwriter.writerow(b)
+    f.close()
+    
 def slct():
     return
 
-def insert():
-    return
+def insert(a, b = "", c = 1):
+    if c == 0:
+        write(a, b)
+        return
+
+    if c == 1 and a[1] == "into":
+        write(a[2], a[3:])
 
 def create(args):
 
@@ -17,10 +33,16 @@ def create(args):
         name = cm_par[2]
         csv_open(name)
         print ("Table ", name, " created")
+
+        if len(cm_par) > 3:
+            headers = cm_par[3:]
+            insert(name, headers, 0)
         return 
     if cm_par[1] == "db":
         print("Not yet availabe")
         return
+
+    
 
 exitcode = 0
 
